a0114c2f7dd04349122bc0f0c545026a292e8f05
CD-Dev1 <0xchris.eth@gmail.com>
2024-02-09 18:38:01 +0000

    sample add and validate employee

--- pages/employeesPage.ts
+++ pages/employeesPage.ts
@@ -9,4 +9,85 @@ export class EmployeePage extends Base {
     })
     await expect(headerText).toBeVisible()
   }
+
+  async selectAddEmployee() {
+    await this.page.getByRole('button', { name: 'Add employee' }).click()
+  }
+
+  async enterInvalidEmployeeDetails(
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+    jobTitle: string
+  ) {
+    const saveEmployeeBtn = await this.page.getByRole('button', {
+      name: 'Save new employee',
+    })
+    await expect(saveEmployeeBtn).toBeDisabled()
+    await this.page.fill('#firstName', firstName)
+    await expect(saveEmployeeBtn).toBeDisabled()
+    await this.page.fill('#lastName', lastName)
+    await expect(saveEmployeeBtn).toBeDisabled()
+    await this.page.fill('#email', email)
+    await expect(saveEmployeeBtn).toBeEnabled()
+    await this.page.fill('#phoneNumber', phoneNumber)
+    await this.page.getByTestId('input-selector').click()
+    await this.page.getByText('22').click()
+    await this.page.fill('#jobTitle', jobTitle)
+    //await saveEmployeeBtn.click();
+  }
+
+  async enterEmployeeDetails(
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+    jobTitle: string
+  ) {
+    const saveEmployeeBtn = await this.page.getByRole('button', {
+      name: 'Save new employee',
+    })
+    await this.page.fill('#firstName', firstName)
+    await this.page.fill('#lastName', lastName)
+    await this.page.fill('#email', email)
+    await this.page.fill('#phoneNumber', phoneNumber)
+    await this.page.getByTestId('input-selector').click()
+    await this.page.getByText('22').click()
+    await this.page.fill('#jobTitle', jobTitle)
+    //await saveEmployeeBtn.click();
+  }
+
+  async validateSuccessModal(firstName: string, lastName: string) {
+    const successMessage = await this.page.getByRole('heading', {
+      name: 'Success! New employee added',
+      exact: true,
+    })
+    await expect(successMessage).toBeVisible()
+
+    const employeeAddedHeader = await this.page.getByText(
+      firstName + ' added to BrightHR Lite'
+    )
+    await expect(employeeAddedHeader).toBeVisible()
+
+    const employeedAddedInfo = await this.page.getByRole('heading', {
+      name: 'employee2 second',
+      exact: true,
+    })
+    await expect(employeedAddedInfo).toBeVisible()
+  }
+
+  async addAnotherEmployee() {
+    await this.page.getByRole('button', {
+      name: 'Add another employee',
+      exact: true,
+    })
+  }
+
+  async validateNewEmployeeDisplayed(firstName: string, lastName: string) {
+    await this.page.getByRole('heading', {
+      name: firstName + ' ' + lastName,
+      exact: true,
+    })
+  }
 }
--- tests/addEmployees.spec.ts
+++ tests/addEmployees.spec.ts
@@ -1,9 +1,76 @@
 import test from '../fixtures/pageFixtures'
+import { DashboardPage } from '../pages/dashboardPage'
+import { employees } from '../data/sampleemployees.json'
 
-test('Login to Account', async ({ homepage, loginpage }) => {
+test('Validation of Add Employee Form', async ({
+  page,
+  homepage,
+  loginpage,
+  dashboardpage,
+  employeepage,
+}) => {
   await homepage.navigateTo('/')
   await loginpage.enterEmail(process.env.ACC_EMAIL!)
   await loginpage.enterPassword(process.env.ACC_PASSWORD!)
   await loginpage.selectLogin()
   await homepage.validateURL('https://sandbox-app.brighthr.com/dashboard')
+  await dashboardpage.selectEmployees()
+  await employeepage.validatePage()
+  await employeepage.selectAddEmployee()
+
+  await employeepage.enterInvalidEmployeeDetails(
+    employees[0].firstName,
+    employees[0].lastName,
+    employees[0].email,
+    employees[0].phoneNumber,
+    employees[0].jobTitle
+  )
+  await page.waitForTimeout(5000)
+})
+
+test('Add employees to App', async ({
+  page,
+  homepage,
+  loginpage,
+  dashboardpage,
+  employeepage,
+}) => {
+  await homepage.navigateTo('/')
+  await loginpage.enterEmail(process.env.ACC_EMAIL!)
+  await loginpage.enterPassword(process.env.ACC_PASSWORD!)
+  await loginpage.selectLogin()
+  await homepage.validateURL('https://sandbox-app.brighthr.com/dashboard')
+  await dashboardpage.selectEmployees()
+  await employeepage.validatePage()
+  await employeepage.selectAddEmployee()
+  await employeepage.enterEmployeeDetails(
+    employees[0].firstName,
+    employees[0].lastName,
+    employees[0].email,
+    employees[0].phoneNumber,
+    employees[0].jobTitle
+  )
+  await page.waitForTimeout(5000)
+})
+
+test('Validate List of employees', async ({
+  page,
+  homepage,
+  loginpage,
+  dashboardpage,
+  employeepage,
+}) => {
+  await homepage.navigateTo('/')
+  await loginpage.enterEmail(process.env.ACC_EMAIL!)
+  await loginpage.enterPassword(process.env.ACC_PASSWORD!)
+  await loginpage.selectLogin()
+  await homepage.validateURL('https://sandbox-app.brighthr.com/dashboard')
+  await dashboardpage.selectEmployees()
+  await employeepage.validatePage()
+  await employeepage.validateNewEmployeeDisplayed(
+    employees[0].firstName,
+    employees[0].lastName
+  )
+
+  await page.waitForTimeout(5000)
 })
